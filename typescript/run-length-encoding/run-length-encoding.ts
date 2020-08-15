b6e06f95132220b610fdae2b6853431c04a09203
Shaun Gedye <sgedye88@yahoo.com.au>
2020-08-15 23:29:14 +0800

    Added RLE :constructionworker:

---
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -0,0 +1,13 @@
+class RunLengthEncoding {
+
+  static encode(input = ""): string {
+    console.log(input)
+    return input
+  }
+  static decode(input = ""): string {
+    console.log(input)
+    return input
+  }
+}
+
+export default RunLengthEncoding;
